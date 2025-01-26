a00ebcf62c5a646a25d91d92eb58e057a0348696
Calboot <113321314+Calboot@users.noreply.github.com>
2025-01-26 21:14:41 +0800

    一点完善

--- src/main/java/com/ayakacraft/carpetAyakaAddition/utils/CommandUtil.java
+++ src/main/java/com/ayakacraft/carpetAyakaAddition/utils/CommandUtil.java
@@ -7,7 +7,6 @@ import lombok.val;
 import net.minecraft.command.CommandSource;
 import net.minecraft.server.command.ServerCommandSource;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
@@ -22,20 +21,11 @@ public class CommandUtil {
         return CommandSource.suggestMatching(context.getSource().getPlayerNames(), builder);
     }
 
-    public static CompletableFuture<Suggestions> dimensionSuggestionProvider(CommandContext<ServerCommandSource> context, SuggestionsBuilder builder) {
-        val worlds = context.getSource().getServer().getWorlds();
-        val names  = new LinkedList<String>();
-        for (val world : worlds) {
-            names.add(world.getRegistryKey().getValue().toString());
-        }
-        return CommandSource.suggestMatching(names, builder);
-    }
-
     public static CompletableFuture<Suggestions> vec3dSuggestionProvider(CommandContext<ServerCommandSource> context, SuggestionsBuilder builder) {
         val source = context.getSource();
         if (source.isExecutedByPlayer()) {
             val pos = source.getPlayer().getBlockPos();
-            val str = "%f %d %f".formatted(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5);
+            val str = "%.1f %.1f %.1f".formatted(pos.getX() + 0.5, pos.getY()+0.5, pos.getZ() + 0.5);
             return CommandSource.suggestMatching(List.of(str), builder);
         }
         return CommandSource.suggestMatching(List.of(), builder);
